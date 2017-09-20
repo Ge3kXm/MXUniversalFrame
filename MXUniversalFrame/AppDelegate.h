9941f359b73e65ffaa6a5960413fbe96c575aa5b
maRk <max@13322.com>
2017-09-20 11:16:20 +0800

    init project

---
+++ MXUniversalFrame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MXUniversalFrame
+//
+//  Created by maRk on 2017/9/20.
+//  Copyright © 2017年 maRk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MXUniversalFrame/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MXUniversalFrame
+//
+//  Created by maRk on 2017/9/20.
+//  Copyright © 2017年 maRk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
